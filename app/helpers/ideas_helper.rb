920f62533aab3b7edc8e50900484689afff3f708
Sarah McVittie <sarah@dressipi.com>
2013-10-05 14:13:14 +0100

    initial commit

---
+++ app/helpers/ideas_helper.rb
@@ -0,0 +1,5 @@
+module IdeasHelper
+	def format_time(time)
+		time.strftime("%b %e, %l:%M %p")
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsgirls::Application.config.secret_key_base = 'dd7cc952d085e6561cde1eac9a572551ef60d175da6261e72ed1acebb2e02bba86602acbc19fc2d7de70f18334161fcbbea69a3f9e23235933902dcf74228733'
